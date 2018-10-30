31ad185a92c7ac497ee419efedc3c7fc37cd2ca9
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-10-30 14:21:29 -0700

    Update depth-first search and breadth-first search files.

--- 02-depth-first-search/src/main.js
+++ 02-depth-first-search/src/main.js
@@ -9,9 +9,31 @@
 
   Write a recursive algorithm that returns its values as an array, ordered using a depth first search, pre-order strategy.
 */
-module.exports.preOrder = function(root) {
+// module.exports.preOrder = function(root) {
+//
+// };
 
-};
+DFSPreOrder (fn, node = this.root) {
+  if (!node) return null
+  fn(node)
+  if (node.left) this.DFSPreOrder(fn, node.left)
+  if (node.right) this.DFSPreOrder(fn, node.right)
+}
+
+DFSInOrder (fn, node = this.root) {
+  if (!node) return null
+  if (node.left) this.DFSInOrder(fn, node.left)
+  fn(node)
+  if (node.right) this.DFSInOrder(fn, node.right)
+}
+
+DFSPostOrder (fn, node = this.root) {
+  if (!node) return null
+  if (node.left) this.DFSPostOrder(fn, node.left)
+  if (node.right) this.DFSPostOrder(fn, node.right)
+  fn(node)
+}
+
 
 /*
   Given the root of the following tree...
--- 03-breadth-first-search/src/main.js
+++ 03-breadth-first-search/src/main.js
@@ -9,6 +9,17 @@
 
   Write an algorithm that returns its values as an array, ordered using a breadth first search strategy.
 */
+breadthFirstSearch (fn) {
+  if (!this.root) return null
+  const queue = [this.root]
+
+  while(queue.length !== 0) {
+    let node = queue.shift()
+    fn(node)
+    if (node.left) queue.push(node.left)
+    if (node.right) queue.push(node.right)
+  }
+}
 module.exports.bfs = function (root) {
-  
+
 };
